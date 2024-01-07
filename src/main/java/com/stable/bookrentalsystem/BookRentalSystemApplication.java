3f0798726098da0878d98c9438dacb440e292369
kjh <kjh920407@naver.com>
2024-01-07 21:58:36 +0900

    fix: BulkInsertRepository 패키지 수정

--- src/main/java/com/stable/bookrentalsystem/BookRentalSystemApplication.java
+++ src/main/java/com/stable/bookrentalsystem/BookRentalSystemApplication.java
@@ -2,6 +2,8 @@ package com.stable.bookrentalsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @EnableJpaAuditing
--- src/main/java/com/stable/bookrentalsystem/repository/BulkInsertRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stable.bookrentalsystem.repository;
-
-import com.stable.bookrentalsystem.domain.book.entity.Book;
-
-import java.util.List;
-
-public interface BulkInsertRepository {
-    void bulkInsert(List<Book> books);
-}
