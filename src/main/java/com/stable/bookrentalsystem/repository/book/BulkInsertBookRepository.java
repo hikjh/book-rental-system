e8c218717dc7655f527b80005a451acc85abc407
kjh <kjh920407@naver.com>
2024-01-07 21:19:26 +0900

    add: 도서 정보 등록
    - 공공데이터 포털 도서 정보 조회
    - 데이터 변환 후 Bulk Insert 처리

---
+++ src/main/java/com/stable/bookrentalsystem/repository/book/BulkInsertBookRepository.java
@@ -0,0 +1,46 @@
+package com.stable.bookrentalsystem.repository.book;
+
+import com.stable.bookrentalsystem.domain.book.entity.Book;
+import com.stable.bookrentalsystem.repository.BulkInsertRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Slf4j
+@Repository
+public class BulkInsertBookRepository implements BulkInsertRepository {
+
+    private final NamedParameterJdbcTemplate template;
+
+    public BulkInsertBookRepository(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Override
+    public void bulkInsert(List<Book> books) {
+
+        String sql = createBulkInsertSQL();
+        log.info("### sql : {}", sql);
+
+        SqlParameterSource[] params = mapToBookSQLParameterSource(books);
+        log.info("### params : {}", params);
+
+        template.batchUpdate(sql, params);
+    }
+
+    private String createBulkInsertSQL() {
+        return "INSERT INTO book (book_id, title, author, publisher, publish_year, is_rented, created_date, modified_date)" +
+                " VALUES (:id, :title, :author, :publisher, :publishYear, :rented, now(), now())";
+    }
+
+    private SqlParameterSource[] mapToBookSQLParameterSource(List<Book> books) {
+        return books.stream()
+                .map(BeanPropertySqlParameterSource::new)
+                .toArray(SqlParameterSource[]::new);
+    }
+}
